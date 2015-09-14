aafa96b26a89e316597b71c7ee4f535f1d2aaf09
RoyTimes <roytimes@icloud.com>
2015-09-14 17:51:37 +0800

    Update Files

---
+++ Song Zhou#3/assign3/colleges.js
@@ -0,0 +1,17 @@
+// I've included both the universities full names and their nicknames
+// use the nicknames for this assignment.  I've included the full names
+// so those who aren't familiar with Bay Area universities will know
+// what the names of the universities actually are.
+
+var univArray = new Array(
+		{name: "Stanford University", nickname: "Stanford", ownership: "private", SATh: 1570, SATl: 1380, tuition: 44757},
+		{name: "University of California, Berkeley", nickname: "UC Berkeley", ownership: "public", SATh: 1500, SATl: 1250, tuition: 13844},
+		{name: "University of California, Santa Cruz", nickname: "UC Santa Cruz", ownership: "public", SATh: 1280, SATl: 1000, tuition: 13398},
+		{name: "San Francisco State University", nickname: "SFSU", ownership: "public", SATh: 1110, SATl: 880, tuition: 6468},
+		{name: "San Jose State University", nickname: "SJSU", ownership: "public", SATh: 1160, SATl: 880, tuition: 9496},
+		{name: "Sonoma State University", nickname: "Sonoma State", ownership: "public", SATh: 1090, SATl: 880, tuition: 7276},
+		{name: "California State University, East Bay", nickname: "CalState East Bay", ownership: "public", SATh: 1010, SATl: 800, tuition: 6550, room: 6435},
+		{name: "University of San Francisco", nickname: "USF", ownership: "private", SATh: 1270, SATl: 1070, tuition: 41450},
+		{name: "Santa Clara University", nickname: "SCU", ownership: "private", SATh: 1380, SATl: 1190, tuition: 43812},
+		{name: "Mills College", nickname: "Mills College", ownership: "private", SATh: 1250, SATl: 1040, tuition: 42918}
+		);
---
+++ Song Zhou#3/assign3/locations.js
@@ -0,0 +1,14 @@
+// data for optional find location extension to maps assignment
+// x and y coordinates are for map-xl.gif, you should scale them
+// in your program based on the current map resolution
+
+var locationArray = new Array(
+		{names: ["Gates"], x: 1558, y: 1461},
+		{names: ["MemChu","Memorial Church"], x: 1845, y: 1883},
+		{names: ["Tresidder Union", "Tresidder"], x: 1804, y: 2225},		
+		{names: ["Florence Moore Hall", "Florence Moore", "FloMo"], x: 1705, y: 2496},
+		{names: ["Bookstore", "Book Store"], x: 2022, y: 2144},
+		{names: ["MemAud", "Memorial Auditorium", "Memorial Hall"], x: 2262, y: 1600},
+		{names: ["Green Library", "Green"], x: 2173, y: 1898},
+		{names: ["Meyer Library", "Meyer"], x: 2157, y: 2026}			
+	);
